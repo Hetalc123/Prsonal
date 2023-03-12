1de66b2db399e42e9afb894a4ea95292d6887e7c
Hetal c <hetu.chova2014@gmail.com>
2023-03-12 18:00:26 +0000

    first commit

---
+++ src/Personal.java
@@ -0,0 +1,25 @@
+//global static/class variables
+public class Personal {
+    static String name = "Riya";
+    static int age = 28;
+    static String mobileNumber = "07822459165";
+    static String address = "35 london rd E5 8nd London";
+    static int rollNumber = 32;
+    static char grade = 'A';
+    static String eligibility = "True";
+    static double amount = 4.87;
+    //calling class variables to print method
+    public static void main(String[] args){
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(mobileNumber);
+        System.out.println(address);
+        System.out.println(rollNumber);
+        System.out.println(grade);
+        System.out.println(eligibility);
+        System.out.println(amount);
+
+    }
+
+
+}
